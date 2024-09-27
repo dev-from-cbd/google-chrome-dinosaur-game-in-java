120be40d433d946c1ae582aab7bea23c47872333
dev-from-cbd <veryfatwombat@gmail.com>
2024-09-28 08:41:15 +0930

    Game

--- src/App.java
+++ src/App.java
@@ -3,6 +3,11 @@ import javax.swing.*;
 public class App {
     public static void main(String[] args) throws Exception {
         int boardWidth = 750;
-        
+        int boardHeight = 250;
+
+        JFrame frame = new JFrame("Google Chrome Dinosaur Game in Java");
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 }
